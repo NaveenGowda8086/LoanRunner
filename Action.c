1b239933de600356abd2a6c6e8528cf7b4365181
unknown <Admin@DESKTOP-BUEIE9U>
2020-03-29 19:39:12 +0530

    First commit from LR

---
+++ Action.c
@@ -0,0 +1,30 @@
+Action()
+{
+
+	web_set_sockets_option("SSL_VERSION", "AUTO");
+
+	web_add_auto_header("Accept", 
+		"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+
+	web_add_header("Accept-Encoding", 
+		"gzip, deflate");
+
+	web_add_header("Accept-Language", 
+		"en-US,en;q=0.9,kn;q=0.8");
+
+	web_add_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	web_url("blazedemo.com", 
+		"URL=http://blazedemo.com/", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t1.inf", 
+		"Mode=HTML", 
+		EXTRARES, 
+		"Url=/favicon.ico", ENDITEM, 
+		LAST);
+
+	return 0;
+}
